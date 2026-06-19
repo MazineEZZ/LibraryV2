84a8a88e2563a6dd4145f76a5fdef898ff682594
MazineEZZ <mazinewolf2007@gmail.com>
2026-06-19 18:13:52 +0100

    project finished

--- script.js
+++ script.js
@@ -7,9 +7,22 @@ const bookTitle = document.querySelector("#title");
 const bookAuthor = document.querySelector("#author");
 const bookPages = document.querySelector("#pages");
 const bookIsRead = document.querySelector("#read");
-
 const bookSubmit = document.querySelector("#submit");
+const bookForm = document.querySelector("form");
 
+// JS Variables
+let library = [];
+const colors = [
+    "#815756",
+    "#d34245",
+    "#3b796b",
+    "#06532e",
+    "#d2b482",
+    "#f17448",
+    "#76588f",
+    "#0d8cce",
+
+]
 
 openModal.addEventListener("click", () => {
     modal.showModal();
@@ -28,11 +41,12 @@ bookSubmit.addEventListener("click", (e) => {
     const isRead = bookIsRead.checked;
     addBookToLibrary(title, author, pages, isRead);
     displayLibrary();
+
+    bookForm.reset();
+    modal.close();
 })
 
 
-// JS Variables
-const library = [];
 
 function Book(title, author, pages, isRead) {
     this.id = crypto.randomUUID();
@@ -40,6 +54,7 @@ function Book(title, author, pages, isRead) {
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    this.color = Math.floor(Math.random() * colors.length);
 }
 
 function addBookToLibrary(title, author, pages, isRead) {
@@ -58,6 +73,9 @@ function displayLibrary() {
         const bookReadBtn = document.createElement("button");
         bookCard.classList.add("card");
         bookInfo.classList.add("info");
+        bookTitle.classList.add("title");
+        bookAuthor.classList.add("author");
+        bookPages.classList.add("pages");
         bookTitle.innerText = book.title;
         bookAuthor.innerText = book.author;
         bookPages.innerText = book.pages;
@@ -67,16 +85,42 @@ function displayLibrary() {
                 bookReadBtn.classList.add("read");
         }
         bookReadBtn.addEventListener("click", (e) => toggleReadState(e, book));
+        
+        // Remove Button
+        const removeBtn = document.createElement("button");
+        const removeImg = document.createElement("img");
+        removeImg.src = "assets/img/remove.png"
+        removeImg.alt = "Remove Book";
+        removeImg.width = 20;
+        removeImg.height = 20;
+        removeBtn.appendChild(removeImg);
+        removeBtn.addEventListener("click", () => removeBookByID(book.id));
+        removeBtn.classList.add("remove");
+
+        const bookOptions = document.createElement("div");
+        bookOptions.classList.add("options");
+
+        bookInfo.style.backgroundColor = colors[book.color];
+
 
         bookInfo.appendChild(bookTitle);
         bookInfo.appendChild(bookAuthor);
         bookInfo.appendChild(bookPages);
         
+        bookOptions.appendChild(bookReadBtn);
+        bookOptions.appendChild(removeBtn);
+
         bookCard.appendChild(bookInfo);
-        bookCard.appendChild(bookReadBtn);
+        bookCard.appendChild(bookOptions);
 
         bookContainer.appendChild(bookCard);
     }
+}
+
+function removeBookByID(id) {
+    library = library.filter(book => book.id != id);
+
+    displayLibrary();
 }
 
 function toggleReadState(btn, book) {
